d81c6d694a1ba5b3e29942f1f876aed4a8478524
Pendik <Pendik@BIR>
2024-10-08 18:08:54 +0300

    commit attımm

--- ECommerce.WebApi/Controllers/ProductsController.cs
+++ ECommerce.WebApi/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using ECommerce.WebApi.Models;
+﻿using ECommerce.WebApi.Contexts;
+using ECommerce.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,40 @@ namespace ECommerce.WebApi.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
+
+    MsSqlContext context = new MsSqlContext();
+
+
+    [HttpPost("add")]
+
+    public IActionResult Add([FromBody] Product product)
+    {
+
+        // insert into Products(kolonlar) values(değerler)
+        context.Products.Add(product);
+        context.SaveChanges();
+
+        return Ok(product);
+    }
+
+    [HttpGet("getall")]
+ 
+    public IActionResult GetAll()
+    {
+        // sELECT * FROM PRODUCTS
+        return Ok(context.Products.ToList());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute]int id)
+    {
+        Product product = context.Products.SingleOrDefault(x=>x.Id==id);
+        if (product is null)
+        {
+            return NotFound("Aradığınız ürün bulunamadı.");
+        }
+
+        return Ok(product);
+    }
 
 }
